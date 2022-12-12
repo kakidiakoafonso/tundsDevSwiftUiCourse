3f61bc327265487f1862a38fb0410aa5e3de0887
Kakidiako Afonso <kakidiakoafonso.gmail.com>
2022-12-12 10:28:47 +0100

    Initial Commit

---
+++ tundsDevCourse/tundsDevCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  tundsDevCourseApp.swift
+//  tundsDevCourse
+//
+//  Created by Kakidiako Afonso on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct tundsDevCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
